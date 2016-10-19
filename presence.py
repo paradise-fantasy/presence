79270ba0dcadce7590cfa36ffb66aa5c8f90d8b0
hafager <fagervoll@gmail.com>
2016-10-19 21:43:29 +0200

    Flamingo

--- presence.py
+++ presence.py
@@ -5,6 +5,8 @@ import bluetooth
 import time
 from Person import Person
 from bluetooth_handler import check_home
+from mqtt_api import send_presence_event send_everyone_present
+
 
 
 havard		=	Person("Haavard",		"40:B8:37:2C:C6:9F", 	"blue")
@@ -15,6 +17,7 @@ kabbe		=	Person("Jon-Anders",	"08:21:EF:7A:E7:88",	"cyan")
 
 
 list_of_members = [havard, frederik, raymi, tormod, kabbe]
+currently_home = 0
 
 def main():
 	print("[*] Paradise: Who's home?")
@@ -22,6 +25,14 @@ def main():
 
 		for person in list_of_members:
 			check_home(person)
+		counter = 0
+		for deltager in list_of_members:
+			if deltager.isPresent()
+				counter += 1
+		if (counter == 4 && currently_home != 4):
+			send_everyone_present("true")
+		else if (currently_home != 4)
+			currently_home = counter
 
 		time.sleep(2)
 
