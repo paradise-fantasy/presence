cbe43ac0a4dab6c99b6c3a0bb54f9a97f8ec58ff
hafager <fagervoll@gmail.com>
2016-10-19 19:29:13 +0200

    Fix ascii error

--- presence.py
+++ presence.py
@@ -7,7 +7,7 @@ from Person import Person
 from bluetooth_handler import check_home
 
 
-havard		=	Person("Håvard",		"40:B8:37:2C:C6:9F", 	"blue")
+havard		=	Person("Haavard",		"40:B8:37:2C:C6:9F", 	"blue")
 frederik	=	Person("Frederik",		"F0:24:75:73:CE:7F",	"green")
 raymi		=	Person("Raymi",			"84:8E:DF:4B:D7:9F",	"red")
 tormod		=	Person("Tormod",		"F4:8E:92:7F:27:12",	"yellow")
