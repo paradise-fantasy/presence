271237cd7aeb15152e0266b5e7e33c9a48fa273d
hafager <fagervoll@gmail.com>
2016-10-19 19:32:27 +0200

    English. Publish to notifier

--- mqtt_api.py
+++ mqtt_api.py
@@ -1,9 +1,10 @@
 import paho.mqtt.publish as publish
 
-topic = "paradise/presence/data"
-
+data_topic = "paradise/presence/data"
+notify_topic = "paradise/notify/presence"
 
 def send_presence_event(presence_event):
 	data = str(presence_event)
 
-	publish.single(topic, data, port=8883, tls={'ca_certs':"ca.crt",'tls_version':2}, hostname="nyx.bjornhaug.net")
+	publish.single(data_topic, data, port=8883, tls={'ca_certs':"ca.crt",'tls_version':2}, hostname="nyx.bjornhaug.net")
+	publish.single(notify_topic, data, port=8883, tls={'ca_certs':"ca.crt",'tls_version':2}, hostname="nyx.bjornhaug.net")
